47d099dc7192774c18cb36539b4d7e37e514ce10
ddrelk <drelk53@yahoo.com>
2023-06-29 23:42:47 +0100

    Static and templates folder added

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = [
+    {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
+    {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
+    # More blog posts can go here...
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', posts=data)
 
 
 if __name__ == '__main__':
